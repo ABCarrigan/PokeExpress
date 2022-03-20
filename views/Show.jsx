b70cccf943a0178996846144bc7adfc9e4399904
ABCarrigan <98413058+ABCarrigan@users.noreply.github.com>
2022-03-20 12:57:45 -0400

    delete button added

--- views/Show.jsx
+++ views/Show.jsx
@@ -9,8 +9,11 @@ class Show extends React.Component {
                 <h1 className="pokemonName">{pokemon.name}</h1>
                 <img src={`${pokemon.img}.jpg`} id="pokemonImgShow"></img>
                 <p class="pokemonDescription">{pokemon.description}</p>
-                <form action={`/pokemon/${pokemon.id}/edit`}>
+                <form action={`/pokemon/${pokemon._id}/edit`}>
                         <input type="submit" value="Edit" />
+                </form>
+                <form action={`/pokemon/${pokemon._id}?_method=DELETE`} method="POST">
+                            <input type="submit" value="DELETE"/>
                 </form>
                 <a href="/pokemon">Back to the index</a>
             </div>
@@ -19,26 +22,3 @@ class Show extends React.Component {
 }
 
 module.exports = Show
-
-// const React = require('react')
-// const headerStyle = {
-//     color: '#ffffff',
-//     backgroundColor: '#960606',
-// }
-
-// class Show extends React.Component {
-
-//    render () {
-//        const { pokemon } = this.props
-     
-//     return (
-//         <div>
-//             <h1 style ={headerStyle}>Gotta Catch 'Em All!</h1>
-//             <h2 style={{ textTransform: 'capitalize' }}>{pokemon.name}</h2><br/>
-//             <img src={`${pokemon.img}.jpg`}></img><br/>
-//             <a href="/pokemon">Back</a>
-//         </div>
-//      )
-//     }
-//  }
-//  module.exports = Show
